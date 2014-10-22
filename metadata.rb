9620c2aaedc8a7d8c2fad129bd36c30f28a75b80
Jake Plimack <jake.plimack@gmail.com>
2014-10-22 12:17:46 -0600

    set cookbook name and lock into nodejs version

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             "application_nodejs"
 maintainer       "Conrad Kramer"
 maintainer_email "conrad@kramerapps.com"
 license          "Apache 2.0"
@@ -5,7 +6,7 @@ description      "Deploys and configures Node.js applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.0.0"
 
-depends          "nodejs"
+depends          "nodejs", '>= 2.1.0'
 depends          "application"
 
 supports 'ubuntu', ">= 12.04"
--- providers/nodejs.rb
+++ providers/nodejs.rb
@@ -22,7 +22,7 @@ include Chef::Mixin::LanguageIncludeRecipe
 
 action :before_compile do
 
-  include_recipe 'nodejs::install_from_source'
+  include_recipe 'nodejs::nodejs_from_source'
 
   if new_resource.npm
     include_recipe 'nodejs::npm'
